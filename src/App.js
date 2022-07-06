8fbf4833d923a31b2fa3ad0d5d7b81b40a0bc409
Prometheus2k <shonesk2000@gmail.com>
2022-07-06 23:24:16 +0530

    Added Welcome Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
+
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Smart-vote
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<WelcomePage />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/WelcomePage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/WelcomePage.css";
+
+const WelcomePage = () => {
+  return (
+    <div className="welcome-page">
+      <h1>Smart Vote</h1>
+      <h3>Secure Voting Application</h3>
+      <Link to="/login">
+        <button>Voter Login</button>
+      </Link>
+      <Link to="/admin-login">
+        <button>Admin Login</button>
+      </Link>
+    </div>
+  );
+};
+
+export default WelcomePage;
